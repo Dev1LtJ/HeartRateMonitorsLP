a4e7a8289ce194b9c4277e21292c4c63923f4e4c
Aliaksandr Razumny <61558614+Dev1LtJ@users.noreply.github.com>
2020-12-16 17:02:20 +0300

    Added mailer, pageupper, animation

--- src/js/script.js
+++ src/js/script.js
@@ -78,4 +78,36 @@ $(document).ready(function(){
     validateForm('#order form');
 
     $('input[name=phone]').mask("+375 (99) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    //Smooth scroll
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn('slow');
+        } else {
+            $('.pageup').fadeOut('slow');
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
 });
